cd45f2b238f0c1112d38cafed4a3aac070f5b003
avikrayan <avikrayan50@gmail.com>
2016-11-26 19:17:41 +0530

    wrote an intent

--- app/src/main/java/com/example/avikrayan/formyrepo/MainActivity.java
+++ app/src/main/java/com/example/avikrayan/formyrepo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.avikrayan.formyrepo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void helloAshu(){
-        
+        Intent i=new Intent();
+
     }
 }
